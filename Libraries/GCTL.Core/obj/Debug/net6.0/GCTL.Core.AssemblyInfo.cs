9f8ade15fe21477b511e52611a1c5114ea894e9d
mdsamaul <mdsamaul843@gmail.com>
2025-07-05 10:24:41 +0600

    force add loan entry report

--- Libraries/GCTL.Core/obj/Debug/net6.0/GCTL.Core.AssemblyInfo.cs
+++ Libraries/GCTL.Core/obj/Debug/net6.0/GCTL.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GCTL.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84cee5e5727bf593e9d713320dc5c4722210bd07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+878bfd31cc2787173ac34d513e82aec8c8fbe6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GCTL.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GCTL.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
