dc78310258bcfe1f7661f2439c90af7c83b33475
mdsamaul <mdsamaul843@gmail.com>
2025-07-06 13:29:52 +0600

    added branch git rm -r --cached .vs

---
+++ UIs/GCTL.UI.Core/Controllers/PrintingStationeryPurchaseEntryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCTL.UI.Core.Controllers
+{
+    public class PrintingStationeryPurchaseEntryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
